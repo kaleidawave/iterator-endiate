ca302760272bb530b24aceaa12b635af8191b323
Ben <kaleidawave@gmail.com>
2022-12-28 09:04:32 +0000

    Add README + 'nendiate' method

---
+++ README.md
@@ -0,0 +1,5 @@
+# Iterator endiate
+
+Adds two new methods to iterators (specifically only `ExactSizeIterator`s) for evaluating whether the item is the last in the iterator.
+
+Similar to [`Iterator::enumerate`](https://doc.rust-lang.org/stable/std/iter/trait.Iterator.html#method.enumerate) which adds counts to items.
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,14 @@
-pub trait EndiateIteratorExt: Sized {
-    fn endiate(self) -> Endiate<Self>;
-}
+#![doc = include_str!("../README.md")]
 
-impl<Iter: ExactSizeIterator> EndiateIteratorExt for Iter {
+/// Extension trait for adding [EndiateIteratorExt::endiate] and [EndiateIteratorExt::nendiate]
+pub trait EndiateIteratorExt: Sized {
     /// Similar to [`Iterator::enumerate`] but rather including indexes, it includes whether the item
     /// is the last item in the iterator
     /// ```
     /// use iterator_endiate::EndiateIteratorExt;
-    /// let endiated = vec![1, 2, 3].into_iter().endiate().collect::<Vec<_>>();
+    /// let endiate = [1, 2, 3].into_iter().endiate().collect::<Vec<_>>();
     /// assert_eq!(
-    ///     endiated,
+    ///     endiate,
     ///     [
     ///         (false, 1),
     ///         (false, 2),
@@ -17,19 +16,52 @@ impl<Iter: ExactSizeIterator> EndiateIteratorExt for Iter {
     ///     ]
     /// )
     /// ```
+    fn endiate(self) -> Endiate<Self>;
+
+    /// Same as [EndiateIteratorExt::endiate] but bool is **not** at end
+    /// ```
+    /// use iterator_endiate::EndiateIteratorExt;
+    /// let nendiate = [1, 2, 3].into_iter().nendiate().collect::<Vec<_>>();
+    /// assert_eq!(
+    ///     nendiate,
+    ///     [
+    ///         (true, 1),
+    ///         (true, 2),
+    ///         (false, 3),
+    ///     ]
+    /// )
+    /// ```
+    fn nendiate(self) -> NEndiate<Self>;
+}
+
+impl<Iter: ExactSizeIterator> EndiateIteratorExt for Iter {
     fn endiate(self) -> Endiate<Self> {
-        Endiate { iterator: self }
+        Endiate(self)
+    }
+
+    fn nendiate(self) -> NEndiate<Self> {
+        NEndiate(Endiate(self))
     }
 }
 
-pub struct Endiate<Iter> {
-    iterator: Iter,
-}
+/// From [EndiateIteratorExt::endiate]
+pub struct Endiate<Iter>(pub(crate) Iter);
+
+/// From [EndiateIteratorExt::nendiate]
+pub struct NEndiate<Iter>(pub(crate) Endiate<Iter>);
 
 impl<Iter: ExactSizeIterator> Iterator for Endiate<Iter> {
     type Item = (bool, Iter::Item);
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.iterator.next().map(|v| (self.iterator.len() == 0, v))
+        self.0.next().map(|v| (self.0.len() == 0, v))
+    }
+}
+
+impl<Iter: ExactSizeIterator> Iterator for NEndiate<Iter> {
+    type Item = (bool, Iter::Item);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.next().map(|(at_end, v)| (!at_end, v))
     }
 }
